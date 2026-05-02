efc6eb8494489c9ad9fee142da0ae470b0e3a15d
nitishkumar333 <ae.nitishkumar@gmail.com>
2026-05-02 18:19:31 +0530

    remove empty files

--- backend/app/routers/elysia.py
+++
@@ -1,1 +0,0 @@
-# This file has been removed - all real functionality exists in connections.py, schema.py, knowledge.py, query.py, history.py
--- backend/app/routers/ws_handler.py
+++
@@ -1,1 +0,0 @@
-# This file has been removed - see query.py for the REST chat endpoint
